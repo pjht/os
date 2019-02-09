b1506d95ed7b3f3078ebfec996b31d19cc2f9b88
pjht <terppa05@gmail.com>
2019-02-09 14:35:45 -0600

    VGA driver now clears on initialization

--- drivers/i386/vga.c
+++ drivers/i386/vga.c
@@ -1,13 +1,28 @@
 #include <grub/text_fb_info.h>
 #include "../vga.h"
+#define xy_to_mem(x,y) ((x+(y*height))*2)
 char* screen;
 int width;
 int height;
 vga_colors fg_color=VGA_WHITE;
 vga_colors bg_color=VGA_BLACK;
 
+void vga_set_char(int x,int y,char c) {
+  screen[xy_to_mem(x,y)]=c;
+  screen[xy_to_mem(x,y)+1]=(bg_color<<4)|fg_color;
+}
+
 void vga_init(text_fb_info framebuffer_info) {
   screen=framebuffer_info.address;
   width=framebuffer_info.width;
   height=framebuffer_info.height;
+  vga_clear();
+}
+
+void vga_clear() {
+  for (int y=0;y<height;y++) {
+    for (int x=0;x<width;x++) {
+      vga_set_char(x,y,' ');
+    }
+  }
 }
