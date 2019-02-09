601fa5f166e9d885fb7832a040bcf908dc6a45fd
pjht <terppa05@gmail.com>
2019-02-09 14:33:29 -0600

    Read framebuffer info from GRUB

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,18 @@
 #include "../cpu/cpu_init.h"
+#include <grub/text_fb_info.h>
+#include <stdlib.h>
+#include "multiboot.h"
 
-void kmain() {
+void kmain(multiboot_info_t* header) {
   cpu_init();
+  text_fb_info info;
+  if (header->flags&MULTIBOOT_INFO_FRAMEBUFFER_INFO&&header->framebuffer_type==2) {
+    info.address=(char*)((header->framebuffer_addr&0xFFFFFFFF)+0xC0000000);
+    info.width=header->framebuffer_width;
+    info.height=header->framebuffer_height;
+  } else {
+    info.address=(char*)0xC00B8000;
+    info.width=80;
+    info.height=25;
+  }
 }
