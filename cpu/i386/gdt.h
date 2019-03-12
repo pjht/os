2cc5b0b748abe4ab8d3b48a2d1cb0f403224592a
pjht <terppa05@gmail.com>
2019-03-12 17:26:19 -0500

    More work

--- cpu/i386/gdt.h
+++ cpu/i386/gdt.h
@@ -3,5 +3,6 @@
 
 void gdt_init();
 void tss_stack_reset();
-
+void allow_all_ports();
+void block_all_ports();
 #endif
--- drivers/i386/pci.h
+++ drivers/i386/pci.h
@@ -8,6 +8,6 @@
 #define PCI_CONFIG_DATA 0xCFC
 
 pci_dev_common_info* pci_get_dev_info(uint8_t bus,uint8_t device,uint8_t func);
-void pci_set_dev_info(uint8_t bus,uint8_t device,uint8_t func,pci_dev_common_info* inf);
+void pci_set_dev_info(pci_dev_common_info* inf);
 
 #endif
