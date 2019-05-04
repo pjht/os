24ca0aa079bc91366c22d41d645ebe11481ff26e
pjht <terppa05@gmail.com>
2019-05-04 10:40:00 -0500

    Fix sycalls for messages

--- cpu/tasking.h
+++ cpu/tasking.h
@@ -7,6 +7,6 @@ void tasking_init();
 void tasking_yield();
 Task* tasking_createTask(void* eip);
 char isPrivleged(uint32_t pid);
-void send_msg(uint32_t pid,void* msg);
-void* get_msg(uint32_t* sender);
+void tasking_send_msg(uint32_t pid,void* msg);
+void* tasking_get_msg(uint32_t* sender);
 #endif
