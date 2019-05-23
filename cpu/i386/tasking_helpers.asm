f513ee93a2ff40a8f07b7d850b2c817414173244
pjht <terppa05@gmail.com>
2019-05-23 09:44:07 -0500

    Fix task_init
    I confused what was in ecx with what was in ebx. Swapping them fixed it

--- cpu/i386/tasking_helpers.asm
+++ cpu/i386/tasking_helpers.asm
@@ -48,22 +48,18 @@ switch_to_task:
 global task_init
 
 task_init:
-pop ecx
-pop ebx
-cli
-mov ax, 0x23
-mov ds, ax
-mov es, ax
-mov fs, ax
-mov gs, ax
-
-push 0x23
-push ebx
-pushf
-pop eax
-
-or eax, 0x200
-push eax
-push 0x1B
-push ecx
-iret
+  pop ecx
+  pop ebx
+  cli
+  mov ax, 0x23
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov eax, ecx
+  push 0x23
+  push eax
+  pushf
+  push 0x1B
+  push ebx
+  iret
