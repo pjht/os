f68cf49a15bfcdd834f3f17694194f4fe5879a3d
pjht <terppa05@gmail.com>
2019-05-25 16:00:12 -0500

    copy_data and put_data now use load_smap

--- kernel/cpu/i386/address_spaces.c
+++ kernel/cpu/i386/address_spaces.c
@@ -4,18 +4,18 @@ void address_spaces_copy_data(void* cr3, void* data,uint32_t size,void* virt_add
   uint32_t old_cr3;
   asm volatile("movl %%cr3, %%eax; movl %%eax, %0;":"=m"(old_cr3)::"%eax");
   void* phys_addr=virt_to_phys(data);
-  load_address_space((uint32_t)cr3);
+  load_smap((uint32_t)cr3);
   map_pages(virt_addr,phys_addr,(size/4096)+1,1,1);
-  load_address_space(old_cr3);
+  load_smap(old_cr3);
 }
 
 void* address_spaces_put_data(void* cr3, void* data,uint32_t size) {
   uint32_t old_cr3;
   asm volatile("movl %%cr3, %%eax; movl %%eax, %0;":"=m"(old_cr3)::"%eax");
   void* phys_addr=virt_to_phys(data);
-  load_address_space((uint32_t)cr3);
+  load_smap((uint32_t)cr3);
   void* virt_addr=find_free_pages((size/4096)+1)<<12;
   map_pages(virt_addr,phys_addr,(size/4096)+1,1,1);
-  load_address_space(old_cr3);
+  load_smap(old_cr3);
   return virt_addr;
 }
