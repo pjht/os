6fe1db6956370bfec558d9bf4c7c1cb01d4f6219
pjht <terppa05@gmail.com>
2019-06-29 09:21:08 -0500

    Add a case statement for each message type to the VFS

--- vfs/main.c
+++ vfs/main.c
@@ -17,7 +17,29 @@ int main() {
   yield();
   Message msg;
   vfs_message* vfs_msg=get_message(&msg,box);
-  vfs_msg->fd=2;
+  switch (vfs_msg->type) {
+    case VFS_OPEN:
+    vfs_msg->flags=1;
+    break;
+    case VFS_PUTC:
+    vfs_msg->flags=1;
+    break;
+    case VFS_GETC:
+    vfs_msg->flags=1;
+    break;
+    case VFS_CLOSE:
+    vfs_msg->flags=1;
+    break;
+    case VFS_MOUNT:
+    vfs_msg->flags=1;
+    break;
+    case VFS_UMOUNT:
+    vfs_msg->flags=1;
+    break;
+    default:
+    vfs_msg->flags=2;
+    break;
+  }
   mailbox_send_msg(&msg);
   yield();
   for (;;);
