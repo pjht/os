4771173c6cbd541efe465cd55bb85371ac0377bc
pjht <terppa05@gmail.com>
2019-08-31 11:04:30 -0500

    Add VGA code to vga_drv

--- vga_drv/main.c
+++ vga_drv/main.c
@@ -1,3 +1,9 @@
+#include <grub/text_fb_info.h>
+
 int main() {
-  
+  text_fb_info info;
+  info.address=map_phys((void*)0xB8000,10);
+  info.width=80;
+  info.height=25;
+  vga_init(info);
 }
---
+++ vga_drv/ports.c
@@ -0,0 +1,31 @@
+#include <stdint.h>
+
+uint8_t port_byte_in(uint16_t port) {
+  uint8_t result;
+  asm("in %%dx, %%al":"=a"(result):"d"(port));
+  return result;
+}
+
+void port_byte_out(uint16_t port,uint8_t data) {
+  asm("out %%al, %%dx":: "a"(data),"d"(port));
+}
+
+uint16_t port_word_in(uint16_t port) {
+  uint16_t result;
+  asm("in %%dx, %%ax":"=a"(result):"d"(port));
+  return result;
+}
+
+void port_word_out(uint16_t port,uint16_t data) {
+  asm("out %%ax, %%dx":: "a" (data), "d" (port));
+}
+
+uint32_t port_long_in(uint16_t port) {
+  uint32_t result;
+  asm("inl %%dx, %%eax":"=a"(result):"d"(port));
+  return result;
+}
+
+void port_long_out(uint16_t port,uint32_t data) {
+  asm("outl %%eax, %%dx":: "a" (data), "d" (port));
+}
